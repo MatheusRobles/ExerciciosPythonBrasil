b97af67fc02632553c5c9ecc5938978fb5f8df60
MatheusRobles <92039339+MatheusRobles@users.noreply.github.com>
2021-10-25 17:32:37 -0300

    Add files via upload

---
+++ 01_EstruturaSequencial/Exercicio_9.py
@@ -0,0 +1,4 @@
+print("##Conversor de temperatura de Fahrenheit para Celsius##")
+F = float(input("Digite o valor da temperatura em Fahrenheit: \n"))
+C = 5 * ((F-32) / 9)
+print("O valor da temperatura em Celsius é de:", C)
