0db0b5a61122c5c31e761966964510e2e176efcd
MatheusRobles <92039339+MatheusRobles@users.noreply.github.com>
2021-11-08 09:15:05 -0300

    Update Exercicio_24.py

--- 03_EstruturaDeRepetição/Exercicio_24.py
+++ 03_EstruturaDeRepetição/Exercicio_24.py
@@ -1,16 +1,10 @@
 print('### media de N numeros ###')
-continuar = True
-lista = []
-while(continuar):
-  numero = int(input("insira um numero:\n"))
-  lista.append(numero)
-
-  continua = input("Deseja inserir mais numeros?\n[S]Sim / [N]Não\n").upper()
-  if (continua == 'N'):
-    continuar = False
-  else:
-    continuar = True
-
-soma = sum(lista)
-media = soma/(len(lista))
-print('A media dos numeros é {}'.format(media))
+totalNotas=float(input('Informe o total de notas que você quer calcular: '))
+cont=1
+media=soma=0
+while cont <= totalNotas:
+    nota=float(input('Informe a {}ª nota:'.format(cont)))
+    cont+=1
+    soma+=nota
+media=soma/totalNotas
+print('A média é {:.2f}'.format(media))
